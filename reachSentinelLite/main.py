b49ba8fad00196159465c6621afa16cedf8ecad1
Andrew <andrewe456@gmail.com>
2018-04-18 22:36:34 -0700

    Transferred changes from reach_data_processing
    Didn't get the chance to test...

--- reachSentinelLite/main.py
+++ reachSentinelLite/main.py
@@ -20,16 +20,19 @@ from graphs.models import Telemetry, IsLive  # exec(open("main.py").read())
 
 print("\n\n\n")
 print(
-	'             ||---------------------------- * * * -------------------------\\\\\n'\
-	'             ||   ______                    _____                           \\\\\n'\
-	'       ######||   | ___ \           ()     /  ___|                           \\\\\n'\
-	'  ###########||   | |_/ /_ __ _   _ _ _ __ \ `--. _ __   __  _  ___ ___       \\\\\n'\
-	"#############||   | ___ \ '__| | | | | '_ \ `--. \ '_ \ / _\| |/ __/ _ \       \\\\\n"\
-	'   ##########||   | |_/ / |  | |_| | | | | /\__/ / |_) | (_ | | (__| __/       //\n'\
-	'        #####||   \____/|_|   \__,_|_|_| |_\____/| .__/ \__/|_|\___\___|      //\n'\
-	'      #######||                                  | |                         //\n'\
-	'             ||                                  |_|                        //\n'\
-	'             ||---------------------------- * * * -------------------------//\n')
+	'    +----------------------------------------------------------+\n'\
+	'   /                   /                    /                / |\n'\
+	'  /                   /                    /                /  |\n'\
+	' +---------------------------------------------------------+   |\n'\
+	' |  ______                    _____                        |   |\n'\
+	' |  | ___ \           ()     /  ___|                       |   |\n'\
+	' |  | |_/ /_ __ _   _ _ _ __ \ `--. _ __   __  _  ___ ___  |   |\n'\
+	" |  | ___ \ '__| | | | | '_ \ `--. \ '_ \ / _\| |/ __/ _ \ |   |\n"\
+	' |  | |_/ / |  | |_| | | | | /\__/ / |_) | (_ | | (__| __/ |   +\n'\
+	' |  \____/|_|   \__,_|_|_| |_\____/| .__/ \__/|_|\___\___| |  /\n'\
+	' |                                 | |                     | /\n'\
+	' |                                 |_|                     |/\n'\
+	' +------------------------- * * * -------------------------+\n')
 
 print("\n\n\n")
 
@@ -80,14 +83,20 @@ ACCELZ = 3
 GYROX = 4
 GYROY = 5
 GYROZ = 6
-MAGX = 7
-MAGY = 8
-MAGZ = 9
-MAGHEAD = 10
-TEMP = 11
-PRESSURE = 13
-ALTITUDE = 12
-BAROTEMP = 14
+GPSLAT = 7
+GPSLON = 8
+GPSALT = 9
+GPSHOUR = 10
+GPSMIN = 11
+GPSSEC = 12
+#MAGX = 7
+#MAGY = 8
+#MAGZ = 9
+#MAGHEAD = 10
+TEMP = 13
+PRES = 14
+ALTITUDE = 15
+BAROTEMP = 16
 
 tots_not_launch = 1
 count = 0
@@ -102,7 +111,7 @@ txtfile.write('Project Reach Raw Data starting at ' + date)
 ##Main Processing Loop
 while ser.isOpen():
 	#get data
-	dataString = str(ser.readline())
+	dataString = ser.readline()
 	txtfile.write(dataString)
 	dataString = dataString[2:len(dataString)-5]
 	print('Received: ' + dataString)
@@ -129,26 +138,6 @@ while ser.isOpen():
 	'''
 	data = dataString.split(",")
 
-	'''
-	new_data = Telemetry.objects.create(  -------------- * * * -------------- SAVE TO DATABASE
-	timestamp=data[0], 
-	accel_x=data[6], 
-	accel_y= data[7], 
-	accel_z= data[8], 
-	gyro_x=data[3], 
-	gyro_y=data[4], 
-	gyro_z= data[5],
-	barometer=data[1],
-	temp=data[2])
-	new_data.save()
-	'''
-		
-	#had problems with only reading in a few data 
-	if (len(data) < 6):
-		print("not enough data")
-		continue
-	
-		#convert from string type
 	for i in range(len(data)-1):
 		if "NAN" in data[i]:
 			data[i] = 0.0
@@ -175,123 +164,154 @@ while ser.isOpen():
 
 	#for the first few iterations, just take the 
 	#accelerometer data to calibrate the offsets
-	if(count <= 6):
+
+	if(count<6):
+		print('Calibrating...')
+		ACCX_CALIB += data[ACCELX]
+		ACCY_CALIB += data[ACCELY]
+		ACCZ_CALIB += data[ACCELZ]
+		count+=1
+	else:
 		if(count==6):
 			ACCX_CALIB = ACCX_CALIB/6
 			ACCY_CALIB = ACCY_CALIB/6
 			ACCZ_CALIB = ACCZ_CALIB/6
-		else:
-			ACCX_CALIB += float(data[ACCELX])
-			ACCY_CALIB += float(data[ACCELY])
-			ACCZ_CALIB += float(data[ACCELZ])
-		count += 1
-		continue
+			count+=1
+			print('Calibration: ' + str(ACCX_CALIB) + ', ' + str(ACCY_CALIB) + ', ' + str(ACCZ_CALIB))
+	
+		#had problems with only reading in a few data 
+		if (len(data) < 6):
+			print("not enough data")
+			continue
+	
+		#establish spacecraft time
+		if(FIRST == True):
+			FIRST = False
+			oldtime = float(data[TIMESTAMP])
+			#since dt cannot be established, skip
+			#TODO save first data packet's raw data
+			continue
+	
+		#DO STUFF WITH DATA
+		#convert from string type
+		for i in range(len(data)-1):
+			data[i] = float(data[i])
+		'''
+		if (len(data) < 12):
+			gpsRecieved = False
+			print "no coordinates"
+		else: 
+			gpsRecieved = True
+		'''
+		#establish time elapsed
+		dt = (data[TIMESTAMP] - oldtime)/1000 #convert ms to s
+		oldtime = data[TIMESTAMP]
+	
+		#append altitude calculated from pressure
+		#data.append(altitudeCalc(data[1]))
+	
+		#process acceleration
+		myAcceleration = acceleration.findInertialFrameAccel(data[ACCELX], data[ACCELY], data[ACCELZ], data[GYROX], data[GYROY], data[GYROZ], dt, [ACCX_CALIB, ACCY_CALIB, ACCZ_CALIB])
 		
-	#establish spacecraft time
-	if(FIRST == True):
-		FIRST = False
-		oldtime = float(data[TIMESTAMP])
-		#since dt cannot be established, skip
-		#TODO save first data packet's raw data
-		continue
+		#integrate to find velocity
+		velocity = velocity+myAcceleration*dt
 
-	#DO STUFF WITH DATA
-	'''
-	if (len(data) < 12):
-		gpsRecieved = False
-		print "no coordinates"
-	else: 
-		gpsRecieved = True
-	'''
-	#establish time elapsed
-	dt = (data[TIMESTAMP] - oldtime)/1000 #convert ms to s
-	oldtime = data[TIMESTAMP]
+		#integrate to find position
+		position = position + velocity*dt
 
-	#append altitude calculated from pressure
-	#data.append(altitudeCalc(data[1]))
-
-	#process acceleration
-	myAcceleration = acceleration.findInertialFrameAccel(data[ACCELX], data[ACCELY], data[ACCELZ], data[GYROX], data[GYROY], data[GYROZ], dt, [ACCX_CALIB, ACCY_CALIB, ACCZ_CALIB])
-	#acceleration = (data[ACCELX],data[ACCELY],data[ACCELZ])
-	#integrate to find velocity
-	velocity = velocity+myAcceleration*dt
-
-	#integrate to find position
-	position = position + velocity*dt
-
-	#append inertial frame acceleration velocity 
-	#and position data to transmitted data
-	data.append(myAcceleration.item(0))
-	data.append(myAcceleration.item(1))
-	data.append(myAcceleration.item(0)) 
-	data.append(velocity.item(1))
-	data.append(velocity.item(2))
-	data.append(position.item(0))
-	data.append(position.item(1))
-	data.append(position.item(2))
-
-	'''
-	#Process GPS coordinates
-	if data[9] != oldGPSTime:
-		longitude = data[10]
-		latitude = data[11]
-		latDeg = math.floor(latitude)
-		lonDeg = math.floor(longitude)
-		latMin = float(latitude) - latDeg
-		lonMin = float(longitude) - lonDeg
-	    
-		lat = float(latDeg)+float(latMin)/60
-	 	lon = float(lonDeg)-float(lonMin)/60
-	    
-		saveCoor(lon, lat, data[12])
-	    
-	    #speed calculation from gps data:
-		#push the last new value to the old and then set the new value 
-		lat1 = lat2
-	 	lat2 = lat
-		lon1 = lon2
-		lon2 = lon
-		dt = data[9] - oldGPSTime
-		#specific to GPS because GPS not expected as often
-		oldGPSTime = data[9]
-		#calcVelGPS(lat1, lon1, lat2, lon2, dt)
-		print "sending:"
-	'''
+		#append inertial frame acceleration velocity 
+		#and position data to transmitted data
+		data.append(myAcceleration.item(0))
+		data.append(myAcceleration.item(1))
+		data.append(myAcceleration.item(0)) 
+		data.append(velocity.item(0)) 
+		data.append(velocity.item(1))
+		data.append(velocity.item(2))
+		data.append(position.item(0))
+		data.append(position.item(1))
+		data.append(position.item(2))
 	
-	#append absolute time
-	data.append(time.time())
+		
+		#Process GPS coordinates
+		if data[GPSSEC] != oldGPSTime:
+			longitude = data[GPSLON]
+			latitude = data[GPSLAT]
+			latDeg = math.floor(latitude)
+			lonDeg = math.floor(longitude)
 	
-	finalData = ""
-	for i in range(len(data)-1):
-		#print data[i]
-		finalData = finalData + str(data[i]) + ", "
-		'''
-		finalData contents should be as follows, 
-		as a string separated by commas:
-		relative spacecraft time 
-		raw accelX
-		raw accelY
-		raw accelZ
-		gyroX
-		gyroY
-		gyroZ
-		magX
-		magY
-		magZ
-		magHead
-		temp(C)
-		altitude
-		inertial accelX
-		inertial accelY
-		inertial accelZ
-		velX
-		velY
-		velZ
-		posX
-		posY
-		posZ
-		absTime
-		'''
-	print(finalData)
-	#sock.sendto(finalData, (SEND_TO_IP, SEND_TO_PORT))
-#killSocket(sock)
+			latMin = float(latitude) - latDeg
+			lonMin = float(longitude) - lonDeg
+		    
+			lat = float(latDeg)+float(latMin)/60
+			lon = float(lonDeg)-float(lonMin)/60
+		    
+			saveCoor(lon, lat, data[GPSALT])
+		    
+		    #speed calculation from gps data:
+			#push the last new value to the old and then set the new value 
+			lat1 = lat2
+			lat2 = lat
+			lon1 = lon2
+			lon2 = lon
+
+			dt = data[GPSSEC] - oldGPSTime
+			if dt < 0:
+				dt = data[GPSSEC] + 60 - oldGPSTime
+			#specific to GPS because GPS not expected as often
+			oldGPSTime = data[GPSSEC]
+			#calcVelGPS(lat1, lon1, lat2, lon2, dt)
+	
+			print("sending:")
+		
+		
+		#append absolute time
+		data.append(time.time())
+		
+		finalData = ""
+		for i in range(len(data)-1):
+			#print data[i]
+			finalData = finalData + str(data[i]) + ", "
+			'''
+			finalData contents should be as follows, 
+			as a string separated by commas:
+			relative spacecraft time 
+			raw accelX
+			raw accelY
+			raw accelZ
+			gyroX
+			gyroY
+			gyroZ
+			magX
+			magY
+			magZ
+			magHead
+			temp(C)
+			altitude
+			inertial accelX
+			inertial accelY
+			inertial accelZ
+			velX
+			velY
+			velZ
+			posX
+			posY
+			posZ
+			absTime
+			'''
+		print(finalData)
+
+		######SAVE TO DATABASE
+
+	'''
+	new_data = Telemetry.objects.create(  -------------- * * * -------------- SAVE TO DATABASE
+	timestamp=data[0], 
+	accel_x=data[6], 
+	accel_y= data[7], 
+	accel_z= data[8], 
+	gyro_x=data[3], 
+	gyro_y=data[4], 
+	gyro_z= data[5],
+	barometer=data[1],
+	temp=data[2])
+	new_data.save()
+	'''
