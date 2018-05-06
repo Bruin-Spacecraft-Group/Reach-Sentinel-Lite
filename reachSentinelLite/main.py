9234cb00d767a9ed0429f932ca80d726c3f66a8a
Andrew <andrewe456@gmail.com>
2018-05-06 00:10:52 -0700

    Made a few changes
    Finally got rid of the line that automatically calculated a gps velocity

--- reachSentinelLite/main.py
+++ reachSentinelLite/main.py
@@ -18,7 +18,6 @@ django.setup()
 
 from graphs.models import Telemetry, IsLive  # exec(open("main.py").read())
 
-print("\n\n\n")
 print(
 	'    +----------------------------------------------------------+\n'\
 	'   /                   /                    /                / |\n'\
@@ -33,9 +32,6 @@ print(
 	' |                                 | |                     | /\n'\
 	' |                                 |_|                     |/\n'\
 	' +------------------------- * * * -------------------------+\n')
-
-print("\n\n\n")
-
 
 # Dabatase checks
 if IsLive.objects.count() != 0:  # ------------------ * * * ------------------ REQUIRES TESTING
@@ -70,7 +66,6 @@ GPSInit()
 velocity = np.matrix([0,0,0]).T
 position = np.matrix([0,0,0]).T
 
-#TODO function for dynamicaly assessing calibration constants
 ACCX_CALIB = 0
 ACCY_CALIB = 0
 ACCZ_CALIB = 0
@@ -100,6 +95,7 @@ BAROTEMP = 16
 
 tots_not_launch = 1
 count = 0
+dataString = ''
 
 ##create plain text file to save raw data as backup for database
 date = str(datetime.datetime.now())
@@ -110,196 +106,185 @@ txtfile.write('Project Reach Raw Data starting at ' + date)
 
 ##Main Processing Loop
 while ser.isOpen():
-	#get data
-	dataString = ser.readline()
-	txtfile.write(dataString)
-	dataString = dataString[2:len(dataString)-5]
-	print('Received: ' + dataString)
-	'''
-	LAST YEAR'S ORDER LEFT FOR REFERENCE, IS NOT USED
-	parse string 
-	create array with elements deliminated by spaces
-	contents should be as follows
-	data[0] = timestamp
-	data[1] = pressure
-	data[2] = temperature
-	data[3] = gyroX
-	data[4] = gyroY
-	data[5] = gyroZ
-	data[6] = accelX
-	data[7] = accelY
-	data[8] = accelZ
-	data[9] = gps Time
-	data[10] = lon
-	data[11] = lat
-	data[12] = gpsAlt
-	data[13] = speed
-	data[14] = course
-	'''
-	data = dataString.split(",")
+	try:
+		#get data
+		dataString = ser.readline()
+	except:
+		continue
 
-	for i in range(len(data)-1):
-		if "NAN" in data[i]:
-			data[i] = 0.0
-			continue
-		data[i] = float(data[i])
+	try:
+		txtfile.write(dataString)
+		txtfile.flush()
+	except:
+		pass
 
-		
-	new_data = Telemetry.objects.create(  #-------------- * * * -------------- SAVE TO DATABASE
-		#timestamp=data[TIMESTAMP], 
-		timestamp = datetime.datetime.now(),
-		accel_x=data[ACCELX], 
-		accel_y= data[ACCELY], 
-		accel_z= data[ACCELZ], 
-		gyro_x=data[GYROX], 
-		gyro_y=data[GYROY], 
-		gyro_z= data[GYROZ],
-		#mag_x = data[MAGX],
-		#mag_y = data[MAGY],
-		#mag_z = data[MAGZ],
-		#maghead = data[MAGHEAD],
-		barometer=data[ALTITUDE],
-		temp=data[TEMP])
-	new_data.save()
+	try:
+		#cut off extra characters
+		#first two characters are b'
+		#last 5, I don't actually know....
+		dataString = dataString[2:len(dataString)-5]
+		print('Received: ' + dataString)
+		data = dataString.split(",")
 
-	#for the first few iterations, just take the 
-	#accelerometer data to calibrate the offsets
-
-	if(count<6):
-		print('Calibrating...')
-		ACCX_CALIB += data[ACCELX]
-		ACCY_CALIB += data[ACCELY]
-		ACCZ_CALIB += data[ACCELZ]
-		count+=1
-	else:
-		if(count==6):
-			ACCX_CALIB = ACCX_CALIB/6
-			ACCY_CALIB = ACCY_CALIB/6
-			ACCZ_CALIB = ACCZ_CALIB/6
-			count+=1
-			print('Calibration: ' + str(ACCX_CALIB) + ', ' + str(ACCY_CALIB) + ', ' + str(ACCZ_CALIB))
-	
-		#had problems with only reading in a few data 
-		if (len(data) < 6):
-			print("not enough data")
-			continue
-	
-		#establish spacecraft time
-		if(FIRST == True):
-			FIRST = False
-			oldtime = float(data[TIMESTAMP])
-			#since dt cannot be established, skip
-			#TODO save first data packet's raw data
-			continue
-	
-		#DO STUFF WITH DATA
-		#convert from string type
+		#TODO: check whether this is right/necessary
+		#can have multiple data types in python - no need to change NAN into a float
+		#adjust for NANs
 		for i in range(len(data)-1):
+			if "NAN" in data[i]:
+				#data[i] = 0.0
+				continue
 			data[i] = float(data[i])
-		'''
-		if (len(data) < 12):
-			gpsRecieved = False
-			print "no coordinates"
-		else: 
-			gpsRecieved = True
-		'''
-		#establish time elapsed
-		dt = (data[TIMESTAMP] - oldtime)/1000 #convert ms to s
-		oldtime = data[TIMESTAMP]
-	
-		#append altitude calculated from pressure
-		#data.append(altitudeCalc(data[1]))
-	
-		#process acceleration
-		myAcceleration = acceleration.findInertialFrameAccel(data[ACCELX], data[ACCELY], data[ACCELZ], data[GYROX], data[GYROY], data[GYROZ], dt, [ACCX_CALIB, ACCY_CALIB, ACCZ_CALIB])
-		
-		#integrate to find velocity
-		velocity = velocity+myAcceleration*dt
 
-		#integrate to find position
-		position = position + velocity*dt
+		#Save raw data to database
+		#TODO: Why are we doing this??
+		new_data = Telemetry.objects.create(  #-------------- * * * -------------- SAVE TO DATABASE
+			#timestamp=data[TIMESTAMP], 
+			timestamp = datetime.datetime.now(),
+			accel_x=data[ACCELX], 
+			accel_y= data[ACCELY], 
+			accel_z= data[ACCELZ], 
+			gyro_x=data[GYROX], 
+			gyro_y=data[GYROY], 
+			gyro_z= data[GYROZ],
+			#mag_x = data[MAGX],
+			#mag_y = data[MAGY],
+			#mag_z = data[MAGZ],
+			#maghead = data[MAGHEAD],
+			barometer=data[ALTITUDE],
+			temp=data[TEMP])
+		new_data.save()
 
-		#append inertial frame acceleration velocity 
-		#and position data to transmitted data
-		data.append(myAcceleration.item(0))
-		data.append(myAcceleration.item(1))
-		data.append(myAcceleration.item(0)) 
-		data.append(velocity.item(0)) 
-		data.append(velocity.item(1))
-		data.append(velocity.item(2))
-		data.append(position.item(0))
-		data.append(position.item(1))
-		data.append(position.item(2))
-	
-		
-		#Process GPS coordinates
-		if data[GPSSEC] != oldGPSTime:
-			longitude = data[GPSLON]
-			latitude = data[GPSLAT]
-			latDeg = math.floor(latitude)
-			lonDeg = math.floor(longitude)
-	
-			latMin = float(latitude) - latDeg
-			lonMin = float(longitude) - lonDeg
-		    
-			lat = float(latDeg)+float(latMin)/60
-			lon = float(lonDeg)-float(lonMin)/60
-		    
-			saveCoor(lon, lat, data[GPSALT])
-		    
-		    #speed calculation from gps data:
-			#push the last new value to the old and then set the new value 
-			lat1 = lat2
-			lat2 = lat
-			lon1 = lon2
-			lon2 = lon
+		#for the first few iterations, just take the 
+		#accelerometer data to calibrate the offsets
 
-			dt = data[GPSSEC] - oldGPSTime
-			if dt < 0:
-				dt = data[GPSSEC] + 60 - oldGPSTime
-			#specific to GPS because GPS not expected as often
-			oldGPSTime = data[GPSSEC]
-			#calcVelGPS(lat1, lon1, lat2, lon2, dt)
-	
-			print("sending:")
+		if(count<6):
+			print('Calibrating...')
+			ACCX_CALIB += data[ACCELX]
+			ACCY_CALIB += data[ACCELY]
+			ACCZ_CALIB += data[ACCELZ]
+			count+=1
+			#establish spacecraft time
+			oldtime = float(data[TIMESTAMP])
 		
+		else:
+			if(count==6):
+				ACCX_CALIB = ACCX_CALIB/6
+				ACCY_CALIB = ACCY_CALIB/6
+				ACCZ_CALIB = ACCZ_CALIB/6
+				count+=1
+				print('Calibration: ' + str(ACCX_CALIB) + ', ' + str(ACCY_CALIB) + ', ' + str(ACCZ_CALIB))
 		
-		#append absolute time
-		data.append(time.time())
+			#had problems with only reading in a few data 
+			if (len(data) < 6):
+				print("not enough data")
+				continue
 		
-		finalData = ""
-		for i in range(len(data)-1):
-			#print data[i]
-			finalData = finalData + str(data[i]) + ", "
+			#DO STUFF WITH DATA
 			'''
-			finalData contents should be as follows, 
-			as a string separated by commas:
-			relative spacecraft time 
-			raw accelX
-			raw accelY
-			raw accelZ
-			gyroX
-			gyroY
-			gyroZ
-			magX
-			magY
-			magZ
-			magHead
-			temp(C)
-			altitude
-			inertial accelX
-			inertial accelY
-			inertial accelZ
-			velX
-			velY
-			velZ
-			posX
-			posY
-			posZ
-			absTime
+			if (len(data) < 12):
+				gpsRecieved = False
+				print "no coordinates"
+			else: 
+				gpsRecieved = True
 			'''
-		print(finalData)
+			#establish time elapsed
+			dt = (data[TIMESTAMP] - oldtime)/1000.0 #convert ms to s
+			oldtime = data[TIMESTAMP]
+			'''
+			append altitude calculated from pressure
+			data.append(altitudeCalc(data[1]))
+			'''
+			#process acceleration
+			myAcceleration = acceleration.findInertialFrameAccel(data[ACCELX], data[ACCELY], data[ACCELZ], data[GYROX], data[GYROY], data[GYROZ], dt, [ACCX_CALIB, ACCY_CALIB, ACCZ_CALIB])
+			
+			#integrate to find velocity
+			velocity = velocity + myAcceleration*dt
 
+			#integrate to find position
+			position = position + velocity*dt
+
+			#append inertial frame acceleration velocity 
+			#and position data to transmitted data
+			data.append(myAcceleration.item(0))
+			data.append(myAcceleration.item(1))
+			data.append(myAcceleration.item(0)) 
+			data.append(velocity.item(0)) 
+			data.append(velocity.item(1))
+			data.append(velocity.item(2))
+			data.append(position.item(0))
+			data.append(position.item(1))
+			data.append(position.item(2))
+		
+			
+			#Process GPS coordinates
+			if data[GPSSEC] != oldGPSTime:
+				longitude = data[GPSLON]
+				latitude = data[GPSLAT]
+				latDeg = math.floor(latitude)
+				lonDeg = math.floor(longitude)
+		
+				latMin = float(latitude) - latDeg
+				lonMin = float(longitude) - lonDeg
+			    
+				lat = float(latDeg)+float(latMin)/60
+				lon = float(lonDeg)-float(lonMin)/60
+			    
+				saveCoor(lon, lat, data[GPSALT])
+			    
+			    #speed calculation from gps data:
+				#push the last new value to the old and then set the new value 
+				lat1 = lat2
+				lat2 = lat
+				lon1 = lon2
+				lon2 = lon
+
+				dt = data[GPSSEC] - oldGPSTime
+				if dt < 0:
+					dt = data[GPSSEC] + 60 - oldGPSTime
+				#specific to GPS because GPS not expected as often
+				oldGPSTime = data[GPSSEC]
+				#calcVelGPS(lat1, lon1, lat2, lon2, dt)
+		
+				print("sending:")
+			
+			
+			#append absolute time
+			data.append(time.time())
+			
+			finalData = ""
+			for i in range(len(data)-1):
+				#print data[i]
+				finalData = finalData + str(data[i]) + ", "
+				'''
+				finalData contents should be as follows, 
+				as a string separated by commas:
+				relative spacecraft time 
+				raw accelX
+				raw accelY
+				raw accelZ
+				gyroX
+				gyroY
+				gyroZ
+				magX
+				magY
+				magZ
+				magHead
+				temp(C)
+				altitude
+				inertial accelX
+				inertial accelY
+				inertial accelZ
+				velX
+				velY
+				velZ
+				posX
+				posY
+				posZ
+				absTime
+				'''
+			print(finalData)
+	except:
+		pass
 		######SAVE TO DATABASE
 
 	'''
