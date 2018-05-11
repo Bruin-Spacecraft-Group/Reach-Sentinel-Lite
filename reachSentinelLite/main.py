ffb4bbb1a876131cc74e7a9f01b6040dfef7645e
Andrew <andrewe456@gmail.com>
2018-05-10 20:14:09 -0700

    Final push to Functions
    I believe my work here is done. All that's left is the web integration.

--- reachSentinelLite/main.py
+++ reachSentinelLite/main.py
@@ -11,6 +11,7 @@ from gps.GPS import GPSInit, saveCoor, processCoordinates, calcVelGPS
 #from communication.sendUDP import initSocket, sendPacket, killSocket
 from altimeter.altitudeCalculation import altitudeCalc
 import acceleration
+from serialStuff import openSerialPort, readFromSerial 
 from DataObject import DataObject
 #from accel import findInertialFrameAccel
 
@@ -42,22 +43,7 @@ if IsLive.objects.count() != 0:  # ------------------ * * * ------------------ R
 downlink = IsLive.objects.create() # ----------------- * * * ----------------- INITIALLY FALSE, ON BUTTON-CLICK IN DASH, TRUE
 
 #Open serial port to recieve downlinked data
-try:
-	print("Opening Serial Port...")
-	#initiate serial port to read data from
-	SERIAL_PORT = 'COM4'
-	ser = serial.Serial(
-	    port=SERIAL_PORT,
-	    baudrate=9600,
-	    timeout=5, #give up reading after 5 seconds
-	    parity=serial.PARITY_ODD,
-	    stopbits=serial.STOPBITS_TWO,
-	    bytesize=serial.SEVENBITS
-	)
-	print("connected to port " + SERIAL_PORT)
-except:
-	print("<== Error connecting to " + SERIAL_PORT + " ==>")
-	#exit()
+ser = openSerialPort('COM4')
 
 #set positions of data in incoming csv packet
 TIMESTAMP = 0
@@ -84,6 +70,7 @@ BAROTEMP = 16
 
 ##Initiate variables
 FIRST = True
+oldtime = 0
 oldGPSTime = 0
 GPSInit()
 myData = DataObject()
@@ -101,74 +88,33 @@ txtfile.write('Project Reach Raw Data starting at ' + date)
 
 print('Telemetry initiated')
 
+#Calibrate inertial gravity vector
+myData.calibrate(ser, txtfile, [ACCELX, ACCELY, ACCELZ])
+
 ##Main Processing Loop
-while ser.isOpen():
-	
-	'''if ser.isOpen():
-					print('ser is open')
-					pass
-				else:
-					print("serial is not open")
-					continue'''
+while True:
+	data = readFromSerial(ser, txtfile)
+	if data == -1: continue
 
 	try:
-		#get data
-		print('reading...')
-		dataString = str(ser.readline())
-		print(dataString)
-		'''
-		if(dataString == "b''"):
-			dropped += 1
-		'''
-	except:
-		print("could not read")
-		continue
-
-	try:
-		print('writing to text file')
-		txtfile.write(dataString)
-		txtfile.flush()
-	except:
-		pass
-
-	try:
-		#cut off extra characters
-		#first two characters are b'
-		#last 5, are \r\n'
-		dataString = dataString[2:len(dataString)-5]
-		print('Received: ' + dataString)
-		data = dataString.split(",")
-		#had problems with only reading in a few data 
-		if (len(data) < 6):
-			print("not enough data")
-			continue
-		
-		#adjust for NANs
-		print('checking for NANs')
-		nans = 0
-		for i in range(len(data)-1):
-			if "NAN" in data[i]:
-				data[i] = float('nan')
-				nans += 1
-				continue
-			data[i] = float(data[i])
-		print('NANs: ' + str(nans))
-
 		#Populate data object
 		print('Populating data object...')
 		myData.timestamp = data[TIMESTAMP]
 		myData.accel_x = data[ACCELX]
 		myData.accel_y = data[ACCELY]
 		myData.accel_z = data[ACCELZ]
+		print('step 1')
 		myData.gyro_x = data[GYROX]
 		myData.gyro_y = data[GYROY]
 		myData.gyro_z = data[GYROZ]
+		print('step 2')
 		myData.gps_lat = data[GPSLON]
 		myData.gps_lon = data[GPSLAT]
 		myData.gps_alt = data[GPSALT]
 		myData.gps_hour = data[GPSHOUR]
 		myData.gps_min = data[GPSMIN]
 		myData.gps_sec = data[GPSSEC]
+		print('step 3')
 		#myData.mag_x = data[MAGX]
 		#myData.mag_y = data[MAGY]
 		#myData.mag_z = data[MAGZ]
@@ -178,6 +124,7 @@ while ser.isOpen():
 		myData.altitude = data[ALTITUDE]
 		myData.baro_temp = data[BAROTEMP]
 
+		print('populated')
 		'''
 		#Save raw data to database
 		#TODO: Why are we doing this??
@@ -198,92 +145,42 @@ while ser.isOpen():
 			temp=data[TEMP])
 		new_data.save()
 		'''
+		###PROCESS
+		#establish time elapsed
+		dt = (myData.timestamp - oldtime)/1000.0 #convert ms to s
+		print(myData.timestamp)
+		print(oldtime)
+		oldtime = myData.timestamp
+		print(str(dt))
+		'''
+		append altitude calculated from pressure
+		data.append(altitudeCalc(data[1]))
+		'''
+		#process acceleration
+		acceleration.findInertialFrameAccel(myData, dt)
+		print('found acceleration')
+		#integrate to find velocity and positino
+		acceleration.calculateVelocityAndPosition(myData, dt)
+		print('found vel and pos')
+		#Process GPS coordinates
+		if myData.gps_sec != oldGPSTime:
+			oldGPSTime = myData.gps_sec
+			processCoordinates(myData.gps_lon, myData.gps_lat, myData.gps_alt)
+			print('processed coordinates')
+		'''
+		#append absolute time
+		data.append(time.time())
+		'''
+		myData.printData()
+		#print(dropped)
 
-		#for the first few iterations, just take the 
-		#accelerometer data to calibrate the offsets
+		######SAVE TO DATABASE
 
-		if(count<6):
-			print('Calibrating...')
-			myData.accx_calib += myData.accel_x
-			myData.accy_calib += myData.accel_y
-			myData.accz_calib += myData.accel_z
-			count+=1
-			#establish spacecraft time
-			oldtime = myData.timestamp
-		
-		else:
-			print('processing...')
-			if(count==6):
-				myData.accx_calib = myData.accx_calib/6
-				myData.accy_calib = myData.accy_calib/6
-				myData.accz_calib = myData.accz_calib/6
-				count+=1
-				myData.printCalibration()
-				#print('Calibration: ' + str(ACCX_CALIB) + ', ' + str(ACCY_CALIB) + ', ' + str(ACCZ_CALIB))
-		
-			##DO STUFF WITH DATA
-			#establish time elapsed
-			dt = (myData.timestamp - oldtime)/1000.0 #convert ms to s
-			oldtime = myData.timestamp
-			print(str(dt))
-			'''
-			append altitude calculated from pressure
-			data.append(altitudeCalc(data[1]))
-			'''
-			#process acceleration
-			acceleration.findInertialFrameAccel(myData, dt)
-			#myAcceleration = acceleration.findInertialFrameAccel(data[ACCELX], data[ACCELY], data[ACCELZ], data[GYROX], data[GYROY], data[GYROZ], dt, [ACCX_CALIB, ACCY_CALIB, ACCZ_CALIB])
-			print('found acceleration')
-			#integrate to find velocity and positino
-			acceleration.calculateVelocityAndPosition(myData, dt)
-			print('found vel and pos')
-			#Process GPS coordinates
-			if myData.gps_sec != oldGPSTime:
-				processCoordinates(myData.gps_lon, myData.gps_lat, myData.gps_alt)
-				print('processed coordinates')
-			'''
-			#append absolute time
-			data.append(time.time())
-			'''
-			finalData = ""
-			for i in range(len(data)-1):
-				#print data[i]
-				finalData = finalData + str(data[i]) + ", "
-				'''
-				finalData contents should be as follows, 
-				as a string separated by commas:
-				relative spacecraft time 
-				raw accelX
-				raw accelY
-				raw accelZ
-				gyroX
-				gyroY
-				gyroZ
-				magX - NO
-				magY - NO
-				magZ - NO
-				magHead - NO
-				temp(C)
-				altitude
-				inertial accelX
-				inertial accelY
-				inertial accelZ
-				velX
-				velY
-				velZ
-				posX
-				posY
-				posZ
-				absTime - NO
-				'''
-			print(finalData)
-			myData.printData()
-			print(dropped)
 	except:
 		print('failed\n')
-		print(dropped)
+		#print(dropped)
 		continue #maybe this should be a continue?
-		######SAVE TO DATABASE
+		
 
 	#TODO -- might actually want to implement the data object. 
 	#At this point it's confusing where the processed data ends up
