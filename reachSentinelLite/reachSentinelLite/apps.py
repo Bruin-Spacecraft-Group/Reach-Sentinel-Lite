cc56bf30a23fc9c2403b2661de6cd4156bd2517b
Karthik Pullela <karthik.pullelas@gmail.com>
2020-01-20 14:26:27 -0800

    karthik: Added new UI coloring, testing it out.

---
+++ reachSentinelLite/reachSentinelLite/apps.py
@@ -0,0 +1,4 @@
+from django.apps import AppConfig
+
+class ReachSentinelLiteConfig(AppConfig):
+    name = 'reachSentinelLite'
--- reachSentinelLite/reachSentinelLite/settings.py
+++ reachSentinelLite/reachSentinelLite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'reachSentinelLite.apps.ReachSentinelLiteConfig',
     'testGraph.apps.TestgraphConfig',
     'dashboard.apps.DashboardConfig',
     'graphs.apps.GraphsConfig',
